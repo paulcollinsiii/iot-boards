f25ae961d39df259ed8c10f3c69f28c4edcd527d
Paul Collins <paul.collins.iii@gmail.com>
2021-02-16 20:54:23 -0500

    Safe string added
    This makes it so Serial input doesn't block while the loop is running.
    Also gives me an example of interrupts in TaskManagerIO

--- esp32-humidity/src/main.cpp
+++ esp32-humidity/src/main.cpp
@@ -16,6 +16,7 @@
 #include <CommandParser.h>
 #include <TaskManagerIO.h>
 #include <MQTT.h>
+#include <SafeString.h>
 
 const char* ssid = WIFI_SSID;
 const char* password =  WIFI_PW;
@@ -24,6 +25,9 @@ const char* ntpServer = "192.168.2.1";
 
 typedef CommandParser<> MyCommandParser;
 MyCommandParser parser;
+
+createSafeString(input, 5);
+createSafeString(token, 5);
 
 Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
 Adafruit_SSD1675 display(250, 122, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
@@ -35,16 +39,52 @@ sensors_event_t humidity, temp;
 
 
 
-void cmd_getdata(MyCommandParser::Argument *args, char *response){
-  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
-}
+class SerialCommandEvent : public BaseEvent {
+  private:
+    bool skipToDelim = false;
+
+  public:
+    uint32_t timeOfNextCheck() override {
+      if(input.read(Serial)){
+        markTriggeredAndNotify();
+      }
+      return 100UL * 1000UL; // every 100ms check for serial data
+    }
+
+    void exec() override {
+      char response[MyCommandParser::MAX_RESPONSE_SIZE];
+
+      if(skipToDelim){
+        if(input.endsWithCharFrom("\r\n")){
+          skipToDelim = false;
+        }
+        input.clear();
+        return;
+      }
+      if(input.nextToken(token, "\r\n")){
+        token.debug("after next token=> ");
+        parser.processCommand(token.c_str(), response);
+        return;
+      }
+      if(input.isFull()){
+        skipToDelim = true;
+        input.clear();
+      }
+    }
+
+} serialCommandEvent;
+
+
 
 void publish_serial(){
   Serial.print("Current IP: "); Serial.println(WiFi.localIP());
   Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
   Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" C");
   Serial.print("Current time: "); Serial.println(nyc.dateTime());
+}
+
+void cmd_getdata(MyCommandParser::Argument *args, char *response){
+  publish_serial();
 }
 
 void publish_mqtt(){
@@ -85,7 +125,6 @@ void update_sensors(){
 void update_and_publish(){
   update_sensors();
   publish_mqtt();
-  publish_serial();
 }
 
 void setup_wifi(){
@@ -111,6 +150,9 @@ void setup_mqtt(){
   client.begin("192.168.2.164", net);
 }
 
+void setup_serial(){
+  SafeString::setOutput(Serial);
+}
 void setup_time(){
   Serial.println("Setting up time...");
   nyc.setLocation("America/New_York");
@@ -125,6 +167,7 @@ void setup() {
   Serial.begin(115200);
   display.begin();
 
+  setup_serial();
   setup_shtc3();
   setup_wifi();
   setup_mqtt();
@@ -135,6 +178,7 @@ void setup() {
   parser.registerCommand("read", "", cmd_getdata);
 
   taskManager.scheduleFixedRate(2, update_and_publish, TIME_SECONDS);
+  taskManager.registerEvent(&serialCommandEvent);
   //taskManager.scheduleFixedRate(180, update_display, TIME_SECONDS);
   publish_serial();
 }
