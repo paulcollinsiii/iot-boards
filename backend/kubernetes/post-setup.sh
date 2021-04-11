669f623bc927e6a87ad276006521d03c60b35413
Paul Collins <paul.collins.iii@gmail.com>
2021-04-11 00:05:57 -0400

    WIP Kubernetes standup automation validated

--- backend/kubernetes/post-setup.sh
+++ backend/kubernetes/post-setup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-kubectl get secret kaffi-homelab-ca -o jsonpath="{.data.ca\.crt}" | base64 -d - > ca.crt
+export KUBECONFIG=$(pwd)/admin.conf
+k0sctl kubeconfig >| admin.conf
+kubectl apply -k ./kustomize/env/dev && \
+  kubectl wait --for=condition=ready certificate kaffi-homelab-ca && \
+  kubectl get secret kaffi-homelab-ca -o jsonpath="{.data.ca\.crt}" | base64 -d - > ca.crt
 vagrant ssh -- "sudo /bin/sh -c \"cp /vagrant/ca.crt /usr/local/share/ca-certificates && update-ca-certificates\""
+helm install kaffi-registry ./charts/kaffi-registry
+helm install eclipse-mqtt ./charts/eclipse-mqtt -n iot --create-namespace
