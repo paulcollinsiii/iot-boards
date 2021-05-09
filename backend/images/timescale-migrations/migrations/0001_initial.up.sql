b23075cc8583005cb7efcfe083e65297cf5dfbfa
Paul Collins <paul.collins.iii@gmail.com>
2021-05-08 21:20:53 -0400

    Kubernetes automation cleanup
    This is still somewhat WIP as I was trying to get TimeScaleDB to run
    in cluster. I'm likely going to give up on that because running a DB
    inside a Kube cluster is just a bad plan anyways. I'm spending more time
    futzing with getting the DB in there than I am automating the schema
    definition and getting data written to the thing.

--- backend/images/timescale-migrations/migrations/0001_initial.up.sql
+++ backend/images/timescale-migrations/migrations/0001_initial.up.sql
@@ -1,9 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
-CREATE TABLE sensor_data_defined (
+CREATE TABLE sensor_data (
   time TIMESTAMPTZ NOT NULL,
-  user_id INT,
-  device_id INT,
-  humidity NUMERIC(6, 3) CHECK (humidity >= 0),
-  temprature NUMERIC(10, 4)
+  device_id UUID,
+  data jsonb
 );
