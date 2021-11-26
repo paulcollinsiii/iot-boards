ddb98cdb9de9710ce1b16dfc28d3776bf166d2a0
Paul Collins <paul.collins.iii@gmail.com>
2021-11-25 22:32:53 -0500

    Sensor logging fixes
    * Update header docstring
    * Add timestamps to messages :sweat_smile:
    * Update Log messages / levels

--- esp-idf-humidity/include/sensor.h
+++ esp-idf-humidity/include/sensor.h
@@ -11,6 +11,13 @@ typedef struct {
   time_t timestamp;
 } sensor_data_t;
 
+/**
+ * @brief Dump sensor data struct into JSON
+ *
+ * @return
+ *  - ESP_OK: Success
+ *  - ESP_FAIL: Failed to data queue
+ */
 esp_err_t sensor_jsonify(cJSON *root);
 esp_err_t sensor_start();
 esp_err_t sensor_stop();
